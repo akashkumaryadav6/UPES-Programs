a00b73abbbba453747f09eb9980cb1bf7f7e20d1
akashkumaryadav6 <akashrya24@gmail.com>
2023-09-06 09:34:51 +0530

    commit

--- C/CLASS/22AUG_addtwomatrices.c
+++ C/CLASS/22AUG_addtwomatrices.c
@@ -7,7 +7,7 @@ int main(){
     //printing MATRIX 1
     printf("Enter the number of rows (m): ");
     scanf("%d",&m);
-    printf("Enter the number of rows (n): ");
+    printf("Enter the number of cols (n): ");
     scanf("%d",&n);
     printf("Enter the elements: \n");
     for (int i=0; i<m; i++){
@@ -26,18 +26,18 @@ int main(){
     //printing MATRIX 2
     printf("Enter the number of rows (m): ");
     scanf("%d",&o);
-    printf("Enter the number of rows (n): ");
+    printf("Enter the number of cols (n): ");
     scanf("%d",&p);
     printf("Enter the elements: \n");
     for (int i=0; i<o; i++){
         for (int j=0; j<p; j++){
-            scanf("%d",&arr[i][j]);
+            scanf("%d",&arr2[i][j]);
         }
     }
     printf("\n");
     for (int i=0; i<o; i++){
         for (int j=0; j<p; j++){
-            printf("%d ",arr[i][j]);
+            printf("%d ",arr2[i][j]);
         }
         printf("\n");
     }
